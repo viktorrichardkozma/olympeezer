dad4877534831f3e593b7aeac75ceded70f0befe
Viktor Richard Kozma <kozma.viktor.richard@gmail.com>
2022-12-04 12:58:56 +0100

    Updates

--- src/utils/timeFormatter.tsx
+++
@@ -1,8 +0,0 @@
-const time = (time: number) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time - minutes * 60);
-
-    return `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`
-}
-
-export default time;
